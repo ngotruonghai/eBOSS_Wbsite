e090c0149efbb037ece7ae7515a5dd6cdc6834b3
DESKTOP-9TD4M53\HaiNT126 <HaiNT126>
2023-11-05 18:53:03 +0700

    no message

---
+++ eBOSS_Wbsite/eBOSS_Wbsite/eBOSS_Wbsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eBOSS_Wbsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

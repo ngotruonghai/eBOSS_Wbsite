da63bb07a80aaab98c7c769f55eef897662847cf
DESKTOP-9TD4M53\HaiNT126 <HaiNT126>
2023-11-14 00:47:38 +0700

    test component

---
+++ eBOSS_Wbsite/eBOSS_Wbsite/eBOSS_Wbsite/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eBOSS_WebSite.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult component()
+        {
+            return PartialView();
+        }
+
+        public IActionResult component2()
+        {
+            return PartialView();
+        }
+
+     
+    }
+}
